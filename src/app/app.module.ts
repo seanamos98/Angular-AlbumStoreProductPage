51e28ba3855a2df3024cdbf7eb65948a9b03cd43
Tosin <seanamos98@gmail.com>
2020-10-02 02:49:55 +0100

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
-import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
@@ -10,21 +9,12 @@ import { ProductDescriptionComponent } from "./product-description/product-descr
 import { ProductService } from "./product.service";
 import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
 import { ProductListComponent } from "./product-list/product-list.component";
+import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-  {
-    path: "products",
-    component: ProductListComponent,
-  },
-  {
-    path: "product/:id",
-    component: ProductPageComponent,
-  },
-  {
-    path: "",
-    redirectTo: "products",
-    pathMatch: "full",
-  },
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductPageComponent },
+  { path: "", redirectTo: "products", pathMatch: "full" },
 ];
 
 @NgModule({
